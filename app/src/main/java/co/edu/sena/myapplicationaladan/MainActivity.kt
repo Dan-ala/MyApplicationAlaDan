49e8a373f12bf7e53dafa09522eaf8962e12b3fe
Fernando224-por <andresfernandoxd1591@gmail.com>
2023-10-20 10:20:02 -0500

    Dos texos en columnas

--- app/src/main/java/co/edu/sena/myapplicationaladan/MainActivity.kt
+++ app/src/main/java/co/edu/sena/myapplicationaladan/MainActivity.kt
@@ -3,44 +3,34 @@ package co.edu.sena.myapplicationaladan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import co.edu.sena.myapplicationaladan.ui.theme.MyApplicationAlaDanTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState) //Por medio de esta función, arranca la app
         setContent {
-            MyApplicationAlaDanTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = FMaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Kotlin")
-                }
-            }
-        }
-    }
-}
+            MessageCard(Message("Daniel", "Jetpack Compose")) //Esta función es creada por el usuario
+        } //setContent
+    }//onCreate
+}//MainActivity
+
+data class Message(val author: String, val body: String)
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MessageCard(msg: Message) { //name is the argument
+    Column {
+        Text(text = msg.author) //$ con este símbolo interpolo el nombre de la variable
+        Text(text = msg.body) //$ con este símbolo interpolo el nombre de la variable
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MyApplicationAlaDanTheme {
-        Greeting("Kotlin")
-    }
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Android","Hey, take a look at Jetpack Compose, it's great!")
+    )
 }
